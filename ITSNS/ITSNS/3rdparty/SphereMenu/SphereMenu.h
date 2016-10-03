bf800155bc8aef6ed710029bb36c5cbb9edfef49
huyifan <854152389@qq.com>
2016-10-03 17:42:52 +0800

    ITSNS
    上传第三方框架

---
+++ ITSNS/ITSNS/3rdparty/SphereMenu/SphereMenu.h
@@ -0,0 +1,31 @@
+//
+//  SphereMenu.h
+//  SphereMenu
+//
+//  Created by Tu You on 14-8-24.
+//  Copyright (c) 2014年 TU YOU. All rights reserved.
+//
+
+// 版权属于原作者
+// http://code4app.com (cn) http://code4app.net (en)
+// 发布代码于最专业的源码分享网站: Code4App.com
+
+#import <UIKit/UIKit.h>
+
+@protocol SphereMenuDelegate <NSObject>
+
+- (void)sphereDidSelected:(int)index;
+
+@end
+
+@interface SphereMenu : UIView
+
+@property (weak, nonatomic) id<SphereMenuDelegate> delegate;
+@property (strong, nonatomic) NSMutableArray *items;
+- (instancetype)initWithStartPoint:(CGPoint)startPoint
+                        startImage:(UIImage *)startImage
+                     submenuImages:(NSArray *)images;
+-(void)updateLocation;
+- (void)shrinkSubmenu;
+- (void)expandSubmenu;
+@end
